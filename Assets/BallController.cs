59afb081d7061be75352a310739e7e77c5695f01
24muraa <airbuffalo2014@outlook.jp>
2018-01-18 23:57:23 +0900

    PinBallです

---
+++ Assets/BallController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+    //ボールが見える可能性のあるZ軸の最大値
+    private float vesiblePosZ =- 6.5f;
+
+    //オームオーバーを表示するテキスト
+    private GameObject gameoverText;
+    public Text scoerText;
+    int n;
+
+	// Use this for initialization
+	void Start () {
+        //シーン内のGameOvreTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.vesiblePosZ)
+        {
+            //GameoverTextにゲームオーバーを表示
+            this.gameoverText.GetComponent<Text>().text = "GameOver";
+        }
+	}
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag== "LargeCloudTag")
+        {
+            n += 20;
+            scoerText.text = " "+n;
+        }
+        if(other.gameObject.tag== "LargeStarTag")
+        {
+            n += 20;
+            scoerText.text = "" + n;
+        }
+        if(other.gameObject.tag== "SmallCloudTag")
+        {
+            n += 10;
+            scoerText.text = "" + n;
+        }
+        if(other.gameObject.tag== "SmallStarTag")
+        {
+            n += 10;
+            scoerText.text = "" + n;
+        }
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour {
+
+    // Use this for initialization
+    //HingJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    int w=0;
+	void Start () {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリっパーの傾きを設定
+        SetAngle(this.defaultAngle);
+        w = Screen.width;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //左矢印キーを押したとき左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        //右矢印キーを押したとき右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        //矢印キー話されたときフリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        foreach(Touch touch in Input.touches)
+        {
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if(touch.position.x<w/2 && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                        Debug.Log("a");
+                    }
+                    if (touch.position.x > w / 2 && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    if (touch.position.x < w / 2 && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                    if (touch.position.x > w / 2 && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                    break;
+            }
+        }
+        
+	}
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
